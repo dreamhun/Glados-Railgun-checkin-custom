184de0a15d7da4ddca94f1c9d4d3cba3220052e3
hun <hun@dE3.com>
2026-06-03 19:55:25 +0800

    chore: 更新默认域名和签到调度时间
    1. 注释并移除默认域名中的glados.cloud
    2. 将定时任务从UTC 4、10点调整为UTC 17:30，匹配北京时间凌晨1:30

--- checkin.py
+++ checkin.py
@@ -106,7 +106,8 @@ class Config:
     DEFAULT_VERBOSE = False
 
     """默认域名"""
-    DOMAINS = ["glados.cloud", "railgun.info"]
+    # DOMAINS = ["glados.cloud", "railgun.info"]
+    DOMAINS = ["railgun.info"]
 
     """兑换计划列表"""
     EXCHANGE_PLANS = {
